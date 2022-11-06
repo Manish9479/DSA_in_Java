721eb9bac81553a3f3372c3bee1deb545431aae4
Manish Gupta <manishgupta9479@gmail.com>
2022-11-07 01:09:47 +0530

    3 commit

---
+++ src/Basics/LoopBasic.java
@@ -0,0 +1,20 @@
+package Basics;
+
+public class LoopBasic {
+    public static void main(String[] args) {
+
+
+        int count = 1;
+        while (count != 5) {
+            System.out.println(count);
+            count++;
+
+        }
+
+        //for loop
+        System.out.println("Using for loop");
+        for (int Count = 1; Count !=12  ; Count++){
+            System.out.println(Count);
+        }
+    }
+}
